33c887146f4da250e8fb733a9287efc5f2f0bf45
shs395 <gdj03094@gmail.com>
2024-02-02 22:01:18 +0900

    fix: change DAOForkSupport to false

--- params/config.go
+++ params/config.go
@@ -62,7 +62,7 @@ var (
 		ChainID:                       big.NewInt(8497),
 		HomesteadBlock:                big.NewInt(0),
 		DAOForkBlock:                  big.NewInt(0),
-		DAOForkSupport:                true,
+		DAOForkSupport:                false,
 		EIP150Block:                   big.NewInt(0),
 		EIP150Hash:                    common.Hash{},
 		EIP155Block:                   big.NewInt(0),
